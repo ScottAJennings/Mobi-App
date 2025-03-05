19bc19ddd42697c732f03f966aa88d14f0073b64
Scott Jennings <sc.a.jennings@gmail.com>
2025-03-05 15:16:49 +0100

    Linking database with app via Flask API program

--- mobi_app_app/lib/main.dart
+++ mobi_app_app/lib/main.dart
@@ -1,26 +1,3 @@
-/*
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World! This is my first real change!'),
-        ),
-      ),
-    );
-  }
-}
-*/
-
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
--- mobi_app_app/lib/screens/home_screen.dart
+++ mobi_app_app/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,7 +8,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<String> exercises = ["Push-ups", "Squats", "Lunges", "Plank"];
+  List<String> exercises = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchExercises(); // Fetch data when screen loads
+  }
+
+  // Fetch exercises from Flask API
+  Future<void> fetchExercises() async {
+    final response =
+        await http.get(Uri.parse("http://10.0.2.2:5000/exercises"));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+
+      setState(() {
+        exercises =
+            data.map((exercise) => exercise["name"].toString()).toList();
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+      throw Exception("Failed to load exercises");
+    }
+  }
 
   void addExercise() {
     setState(() {
@@ -21,17 +51,21 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: exercises.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(exercises[index]),
-                  onTap: () {
-                    Navigator.pushNamed(context, '/exercise-description');
-                  },
-                );
-              },
-            ),
+            child: isLoading
+                ? Center(
+                    child:
+                        CircularProgressIndicator()) // Show loading indicator
+                : ListView.builder(
+                    itemCount: exercises.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(exercises[index]),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/exercise-description');
+                        },
+                      );
+                    },
+                  ),
           ),
           ElevatedButton(
             onPressed: () {
